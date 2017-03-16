e685019f506f4ca38cad9e45e47bd835485f14c4
will-r <will@spanner.org>
2017-03-16 13:00:21 +0000

    defaults for grant and award type

--- app/models/award_type.rb
+++ app/models/award_type.rb
@@ -4,6 +4,18 @@ class AwardType
   collection_path "/api/award_types"
   primary_key :code
 
+  def self.new_with_defaults(attributes={})
+    AwardType.new({
+      name: "",
+      code: nil,
+      id_code: nil,  #hack! to allow code to be set by user. TODO: use award_type_id throughout
+      admin_code: "",
+      round_type_name: nil,
+      round_type_slug: nil,
+      round_type_id: nil
+    }.merge(attributes))
+  end
+
   def self.for_selection
     AwardType.all.sort_by(&:name).map{|awt| [awt.short_name, awt.code] }
   end
--- app/models/grant_type.rb
+++ app/models/grant_type.rb
@@ -5,14 +5,18 @@ class GrantType
   primary_key :code
 
   def self.for_selection
-    GrantType.all.sort_by(&:name).map{|grt| [grt.short_name, grt.code] }
+    GrantType.all.sort_by(&:name).map{|grt| [grt.short_name, grt.id] }
   end
   
   def self.new_with_defaults(attributes={})
     GrantType.new({
       name: "",
-      code: "",
+      code: nil,
+      id_code: nil,  #hack! to allow code to be set by user. TODO: use grant_type_id throughout
+      admin_code: "",
       page_collection_id: nil,
+      round_type_name: nil,
+      round_type_slug: nil,
       round_type_id: nil
     }.merge(attributes))
   end
