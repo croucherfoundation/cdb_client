4007f90e7923f6ce58ea7f1d1a18dcabae0c5727
aungkyawphyo <akp@revotechmm.com>
2017-04-04 15:16:00 +0630

    Remove unreleated codes of has_person concern

--- app/models/concerns/has_person.rb
+++ app/models/concerns/has_person.rb
@@ -13,23 +13,7 @@ module HasPerson
       nil
     end
   end
-
-  def person_by_user_uid
-    begin
-      @person ||= Person.where(user_uid: uid).first
-    rescue Her::Errors::Error
-      nil
-    end
-  end
-
-  def person_by_user_uid_present?
-    present_user_uid? && person_by_user_uid.present?
-  end
-
-  def present_user_uid?
-    uid.present?
-  end
-
+    
   def person?
     person_uid? && person.present?
   end
