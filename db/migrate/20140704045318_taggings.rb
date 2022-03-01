97f45d1a6111bc2d0b0563de07ed5dbe3f025c82
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2022-03-01 08:48:34 +0700

    Add condition to check table exists or not

--- db/migrate/20140704045318_taggings.rb
+++ db/migrate/20140704045318_taggings.rb
@@ -1,12 +1,14 @@
 class Taggings < ActiveRecord::Migration[4.2]
   def change
-    create_table :taggings do |t|
-      t.integer :tag_id
-      t.integer :taggee_id
-      t.string :taggee_type
-      t.timestamps
+    unless ActiveRecord::Base.connection.table_exists? 'taggings'
+      create_table :taggings do |t|
+        t.integer :tag_id
+        t.integer :taggee_id
+        t.string :taggee_type
+        t.timestamps
+      end
+      add_index :taggings, :tag_id
+      add_index :taggings, [:taggee_type, :taggee_id], name: "taggee"
     end
-    add_index :taggings, :tag_id
-    add_index :taggings, [:taggee_type, :taggee_id], name: "taggee"
   end
 end
