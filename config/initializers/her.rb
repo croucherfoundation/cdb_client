5e8416bdb72db05860d83b9e35ea6014bcd4d7c1
Zin Moe <dev.zinmoe@gmail.com>
2022-03-24 14:37:09 +0630

    use Typhoeus adapter

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -2,6 +2,7 @@ require 'settings'
 require 'faraday_middleware'
 require 'her'
 require 'her/middleware/json_api_parser'
+require 'typhoeus/adapters/faraday'
 
 api_url = ENV['CORE_API_URL']
 
@@ -9,6 +10,7 @@ CDB = Her::API.new
 CDB.setup url: api_url do |c|
   # Request
   c.use FaradayMiddleware::EncodeJson
+  c.adapter Faraday::Adapter::Typhoeus
   # Response
   c.use Her::Middleware::JsonApiParser
   c.adapter Faraday::Adapter::NetHttp
