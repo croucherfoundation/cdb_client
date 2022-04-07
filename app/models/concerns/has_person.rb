91ab2bd81e9630f450e57865637b6b45cb3b2838
Zin Moe <dev.zinmoe@gmail.com>
2022-04-07 16:36:29 +0630

    remove belongs_to person

--- app/models/concerns/has_person.rb
+++ app/models/concerns/has_person.rb
@@ -2,7 +2,10 @@ module HasPerson
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :person
+    #
+    ## Will be needed to add in Host system
+    # belongs_to :person
+
 
     def person
       @person ||= Person.find(person_uid) if person_uid.present?
