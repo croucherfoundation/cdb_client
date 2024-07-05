307272ae7f17194216f7f83a45aac69aef4fb6b0
Thiha <thiha.devops@gmail.com>
2024-07-05 19:51:01 +0900

    stipend allowance change

--- app/models/stipend_allowance.rb
+++ app/models/stipend_allowance.rb
@@ -8,15 +8,16 @@ class StipendAllowance
   class << self
     def new_with_defaults(attributes={})
       StipendAllowance.new({
-        scholar_per_month: nil,
-        fellow_per_month: nil,
-        sc_studentship_per_month: nil,
-        first_child_per_month: nil,
-        second_child_per_month: nil,
+        scholar_per_annum: nil,
+        fellow_per_annum: nil,
+        sc_studentship_per_annum: nil,
+        first_child_per_annum: nil,
+        second_child_per_annum: nil,
         ra_per_annum: nil,
         ada_per_annum: nil,
         medical_insurance_per_annum: nil,
         arrival_costs: nil,
+        commencement_award: nil,
         year: nil
 
       }.merge(attributes))
