b32637be10ec41528571d7e8ef77220557254d55
Zin Moe <dev.zinmoe@gmail.com>
2026-06-04 17:21:32 +0630

    Add new_with_defaults method to InstitutionAlias for default attribute initialization

--- app/models/institution_alias.rb
+++ app/models/institution_alias.rb
@@ -5,4 +5,15 @@ class InstitutionAlias
   collection_path "/api/institutions/:institution_code/institution_aliases"
 
   belongs_to :institution, foreign_key: :institution_code, primary_key: :code
+
+
+  class << self
+    def new_with_defaults(attributes={})
+      InstitutionAlias.new({
+        alias_name: nil,
+        alias_type: nil
+      }.merge(attributes))
+    end
+  end
+
 end
