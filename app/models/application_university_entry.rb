93cb8eec01dad73e9ec29ed8724a95aca48d3b41
yarzardev <yarzarminwai97@gmail.com>
2026-08-31 13:57:47 +0630

    Add institution type selection to institution picker and update entry methods to handle institution type

--- app/models/application_university_entry.rb
+++ app/models/application_university_entry.rb
@@ -14,19 +14,22 @@ class ApplicationUniversityEntry
       where(application_id: application_id).all
     end
 
-    def create_entry(application_id:, field_type:, user_entered_name:, user_confirmed_name: nil, source_record_id: nil)
+    def create_entry(application_id:, field_type:, user_entered_name:, user_confirmed_name: nil, source_record_id: nil, institution_type: nil)
       create(
         application_id: application_id,
         field_type: field_type,
         user_entered_name: user_entered_name,
         user_confirmed_name: user_confirmed_name,
-        source_record_id: source_record_id
+        source_record_id: source_record_id,
+        institution_type: institution_type
       )
     end
 
-    def update_entry(entry_id:, user_entered_name:)
+    def update_entry(entry_id:, user_entered_name:, institution_type: nil)
       entry = find(entry_id)
-      entry.assign_attributes(user_entered_name: user_entered_name)
+      attributes = { user_entered_name: user_entered_name }
+      attributes[:institution_type] = institution_type if institution_type.present?
+      entry.assign_attributes(attributes)
       entry.save
     end
   end
